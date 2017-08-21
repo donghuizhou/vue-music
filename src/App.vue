76fe6c4e776113c43828e37e76e613461e1aaa39
zhoudonghui <zhoudonghui@superPC>
2017-08-21 17:14:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  date() {},
+  methods: {},
+  mounted() {}
+}
+</script>
+
+<style lang="stylus">
+  @import './common/stylus/variable'
+  #app 
+    color: $color-theme
+</style>
